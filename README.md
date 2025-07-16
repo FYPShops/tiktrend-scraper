1ea6cd913a3d7330f74a232c0dcb14ff1014a514
FYPShops <fahilshops@gmail.com>
2025-07-16 00:36:45 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# TikTrend Scraper v1.0
+
+Uses puppeteer-core for Render compatibility.
+
+## Setup
+```bash
+npm install
+node index.js
+```
---
+++ uploader.js
@@ -0,0 +1,6 @@
+async function uploadToAutoDS(product) {
+  console.log('Uploading to AutoDS:', product.title);
+  // TODO: Add actual AutoDS API call here
+}
+
+module.exports = { uploadToAutoDS };
