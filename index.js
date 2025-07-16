cc1171c00dccecfd3c0aee51be582227e6e4f4aa
FYPShops <fahilshops@gmail.com>
2025-07-16 01:12:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-// Entry point
+process.env.PUPPETEER_EXECUTABLE_PATH = '/usr/bin/chromium-browser';// Entry point
 const { scrapeTrendingProducts } = require('./uploader');
 scrapeTrendingProducts();
