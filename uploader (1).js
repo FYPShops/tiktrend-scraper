4f762771eba5ed09d181801b4824eb39ad22cd74
FYPShops <fahilshops@gmail.com>
2025-07-16 00:51:08 -0700

    Delete uploader (1).js

--- uploader (1).js
+++
@@ -1,6 +0,0 @@
-module.exports = async function uploadToAutoDS(products) {
-  for (const product of products) {
-    console.log(`📤 Uploading: ${product.title} (${product.link})`);
-    // Simulate AutoDS upload logic here
-  }
-};
